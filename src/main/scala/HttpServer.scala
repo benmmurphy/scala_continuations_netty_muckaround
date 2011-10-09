5795fda375eceb4d15b9c20022eaaa182ec9681d
Ben Murphy <benmmurphy@gmail.com>
2011-10-09 12:49:27 +0100

    add port support

--- src/main/scala/HttpServer.scala
+++ src/main/scala/HttpServer.scala
@@ -2,15 +2,17 @@ import org.jboss.netty.bootstrap._
 import org.jboss.netty.channel.socket.nio._
 import java.net._
 import java.util.concurrent._
+import util.Properties
 
 trait HttpServer {
   def handler:Handler
 
   def main(args:Array[String]) : Unit = {
+    val port = Properties.envOrElse("PORT", "8080").toInt
     val bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
       Executors.newCachedThreadPool(),
       Executors.newCachedThreadPool()))
     bootstrap.setPipelineFactory(new HttpServerPipelineFactory(handler))
-    bootstrap.bind(new InetSocketAddress(8080))
+    bootstrap.bind(new InetSocketAddress(port))
   }
 }
